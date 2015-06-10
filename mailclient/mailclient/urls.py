2194d608a210df85c642ac414be9dd92d5cb11e1
drgcaosheng <drgcaosheng@163.com>
2015-06-10 18:09:26 +0800

    hello world
    hello world

--- mailclient/mailclient/urls.py
+++ mailclient/mailclient/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
+    # ('^hello/$',hello),
     # Examples:
     # url(r'^$', 'mailclient.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('mailclient.views',
+    ('^hello/$','hello'),
 )
