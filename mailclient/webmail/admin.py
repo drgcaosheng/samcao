37e2b1da6e7652fc0bd2ab5753aae4adcc3ad265
drgcaosheng <drgcaosheng@163.com>
2015-06-15 00:22:00 +0800

    admin 第六章节
    admin 第六章节

--- mailclient/webmail/admin.py
+++ mailclient/webmail/admin.py
@@ -1,6 +1,29 @@
+#coding:utf-8
 from django.contrib import admin
 from webmail.models import Publisher,Author,Book
 
+#list_display显示列表名称,并且排序
+#search_fields 为搜索框
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','email')
+    search_fields = ('first_name','last_name','email')
+
+#list_display显示列表
+#list_filter右侧搜索日志
+#date_hierarchy顶部日期过滤,接收的是字符串,而不是元组
+#ordering按照所指的列进行排序
+#fields 编辑表单的字段顺序
+#filter_horizontal 添加1对多的多选框
+#raw_id_fields 外键多对多.显示搜索文本框
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','publisher','publication_date')
+    list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
+    #fields = ('title','authors','publisher','publication_date')
+    filter_horizontal = ('authors',)
+    raw_id_fields = ('publisher',)
+    
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Book,BookAdmin)
--- mailclient/webmail/models.py
+++ mailclient/webmail/models.py
@@ -16,12 +16,13 @@ class Publisher(models.Model):
     class Meta:
         ordering = ['name']
 
-#blank=True 可选填
+#blank=True 可选填,verbose_name设置标签显示或者设置为第一个参数.e-mail
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField(blank=True)
+    email = models.EmailField(verbose_name='e-mail',blank=True)
     
+    #组织return 默认返回的相关的信息.
     def __unicode__(self):
         return u"%s %s"%(self.first_name,self.last_name)
 
